76203a977cf5e9be13ba53da32d483a37a5f2d15
Ruedi Lüthi <ruedi.luethi@pro-micron.de>
2024-09-30 10:13:23 +0200

    added documentation for the pca example

--- pages/Experiment_PCA.py
+++ pages/Experiment_PCA.py
@@ -19,13 +19,17 @@ party_colors = {
 }
 
 st.header('Clustering der namentlichen Abstimmungen des 20. Bundestages mittels Principal Component Analysis (PCA)')
-st.write('Datenquelle: https://www.bundestag.de/parlament/plenum/abstimmung/liste')
+st.write('''
+    Datenquelle: https://www.bundestag.de/parlament/plenum/abstimmung/liste     
+    Es wurden alle namentlichen Abstimmungen des 20. Bundestages bis zum 5. Juli 2024 verwendet.
+''')
 
+# fetch and cache data
 @st.cache_data
 def get_data(data_dir):
 
     file_list = os.listdir(data_dir)
-    n = len(file_list) # dimension / amount of votes
+    m = len(file_list) # dimension / amount of votes
 
     p_names = None
 
@@ -40,8 +44,8 @@ def get_data(data_dir):
         if p_names is None:
             p_names = df['Bezeichnung'].tolist()
             parties = df['Fraktion/Gruppe'].tolist()
-            m = len(p_names) # amount of politicians
-            A = np.zeros((m, n))
+            n = len(p_names) # amount of politicians
+            A = np.zeros((n, m))
 
         for index, row in df.iterrows():
             if row['Bezeichnung'] in p_names:
@@ -51,13 +55,55 @@ def get_data(data_dir):
         v_i = A[:, v_index]
         A[:, v_index] = (v_i - np.mean(v_i)) / np.std(v_i)
 
-    return A, m, n, parties
+    return A, n, m, parties
+
+A, n, m, parties = get_data(data_dir)
 
 
-A, m, n, parties = get_data(data_dir)
+st.write(r'''
+    Sei $X_j$ ein Zufallsvektor welcher die $j$-te namentliche Abstimmung repräsentiert.
+    Dabei ist eine Komponenten $x_i \in \left[-1, 0, 1\right]$ die jeweilige Stimme des $i$-ten Politikers.
+    Der Wert $x_i = 1$ bedeutet eine Ja, $x_i = -1$ eine Nein Stimme und $x_i = 0$ eine Enthaltung.
+
+    Die Zufallsvektoren $X_j$ werden mit $\frac{X_j - \mathbb{E}(X_j)}{\text{Var}(X_j)}$ normalisiert so dass $\mathbb{E}(X_j) = 0$ und $\text{Var}(X_j) = 1$ gilt
+    und in der Matrix $A \in \mathbb{R}^{n \times m}$ als Spalten gespeichert.
+    Dabei sind $n$ die Anzahl der Politiker und $m$ die Anzahl der Abstimmungen.
+
+    Das Matrixprodukt $A^\top A$ wird berechnet was der Korrelationsmatrix entspricht:
+''')
+
+st.latex(r'''
+    \text{Corr}(X_j,X_k)
+    = \frac{\mathbb{E} \left( 
+    \left(X_j - \overbrace{\mathbb{E}(X_j)}^{=0} \right) \left(X_k - \overbrace{\mathbb{E}(X_k)}^{=0} \right)\right)}
+    {\sqrt{\underbrace{\text{Var}(X_j)}_{=1} \underbrace{\text{Var}(X_k)}_{=1}}}
+    = \mathbb{E} \left( X_j X_k \right)
+    = \frac{1}{n} a_j^\top a_k
+''')
+
+st.write(r'''
+    Von $A^\top A$ werden die Eigenwerte $\lambda_j$ und Eigenvektoren $v_j$ berechnet.
+    Dann werden die Zeilenektoren $a_i$ von $A$ (alle Abstimmungsresultate eines jeweiligen Politikers) auf die $d = 2$ ersten Eigenvektoren/Hauptachsen projiziert:
+''')
+st.latex(r'''
+    \alpha_i = a_i^\top v_i
+    \quad \Leftrightarrow \quad
+    \tilde{a}_i = A v_i
+    \left(\begin{array}{c}
+        \alpha_1 \\
+        \vdots \\
+        \alpha_i\\
+        \vdots \\
+        \alpha_n
+    \end{array}\right) = A v_i
+''')
+
 
 # calc covariance matrix
-ATA = A.T @ A
+ATA = 1/n * A.T @ A
+
+# st.write(n,m)
+# st.write(ATA.shape)
 
 # get eigenvalues and eigenvectors from covariance matrix
 lambdas, V = np.linalg.eig(ATA)
@@ -88,15 +134,14 @@ lambdas, V = np.linalg.eig(ATA)
 # st.write(U, S, Vh)
 
 # use only two eigenvalues and eigenvectors for 2D plot
-n_ = 2
-Sigma_ = np.zeros((m,n_))
-Sigma_[:n_,:n_] = np.diag(np.sqrt(lambdas[:n_]))
-V_ = V[:,:n_]
+d = 2
+Sigma_ = np.zeros((n,d))
+Sigma_[:d,:d] = np.diag(np.sqrt(lambdas[:d]))
 
 # project the data onto the new 2D basis
-A_ = np.zeros((m,n_))
-for i in range(0,n_):
-    A_[:,i] = A @ V_[:,i]
+A_d = np.zeros((n,d))
+for i in range(0,d):
+    A_d[:,i] = A @ V[:,i]
 
 # plot the data
 fig, ax = plt.subplots()
@@ -107,8 +152,11 @@ for i, party in enumerate(parties):
         legend.append(party)
         label = party
 
-    ax.plot(A_[i,0], A_[i,1], '.', color=party_colors[party], alpha=0.4, label=label)
+    ax.plot(A_d[i,0], A_d[i,1], '.', color=party_colors[party], alpha=0.4, label=label)
 
 # ax.plot(A_[:,0], A_[:,1], 'k.')
 ax.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), frameon=False)
+ax.set_xlabel(r'1. Hauptachse, $\tilde{a}_1$')
+ax.set_ylabel(r'2. Hauptachse, $\tilde{a}_2$')
+ax.set_aspect('equal')
 st.pyplot(fig)
